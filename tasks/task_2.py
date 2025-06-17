4a40415be905d8b289259e81cb73000e4c68fa5e
Aisha <aisha.zhumagul@alumni.nu.edu.kz>
2025-06-17 14:24:57 +0500

    add solutions for tasks 1, 2, 3

--- tasks/task_2.py
+++ tasks/task_2.py
@@ -1,12 +1,15 @@
-# ЗАДАНИЕ 2: Кэширование результатов
-# Напиши декоратор simple_cache, который:
-# - запоминает результат функции при вызове с конкретными аргументами,
-# - при повторном вызове с теми же аргументами — возвращает сохранённый результат без повторного вычисления,
-# - печатает "Из кэша" при использовании кэшированного значения.
-# Подсказка: используй словарь для хранения результатов.
-
 from functools import wraps
 
 
 def simple_cache(func):
     cache = {}
+
+    @wraps(func)
+    def wrapper(*args):
+        if cache.get(func.__name__ + str(args)):
+            print("Из кэша")
+        else:
+            cache[func.__name__ + str(args)] = func(*args)
+        return cache.get(func.__name__ + str(args))
+
+    return wrapper
