fff5f1827f206a44331f481cbbe4ef78e87fdfe6
Aisha <aisha.zhumagul@alumni.nu.edu.kz>
2025-06-17 14:39:27 +0500

    add reformatted solution for task 1

--- tasks/task_1.py
+++ tasks/task_1.py
@@ -4,7 +4,7 @@ from functools import wraps
 def log(func):
     @wraps(func)
     def wrapper(*args, **kwargs):
-        print(f"Вызов: {func.__name__ }{args}")
+        print(f"Вызов: {func.__name__}{args}")
         result = func(*args, **kwargs)
         print(f"Результат: {result}")
         return result
